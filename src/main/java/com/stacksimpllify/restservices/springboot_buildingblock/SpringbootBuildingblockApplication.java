791f330b6996e06060de87601ae730f33a2de960
rakesh0425 <crk.rakesh@gmail.com>
2025-02-25 20:41:01 +0530

    Second commit

--- src/main/java/com/stacksimpllify/restservices/springboot_buildingblock/SpringbootBuildingblockApplication.java
+++ src/main/java/com/stacksimpllify/restservices/springboot_buildingblock/SpringbootBuildingblockApplication.java
@@ -2,7 +2,7 @@ package com.stacksimpllify.restservices.springboot_buildingblock;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//
 @SpringBootApplication
 public class SpringbootBuildingblockApplication {
 
