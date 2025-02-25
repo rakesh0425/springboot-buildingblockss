71e7267a90a678379135fe91b56815e0de8eecfc
rakesh0425 <crk.rakesh@gmail.com>
2025-02-25 20:14:55 +0530

    First Commit

---
+++ src/test/java/com/stacksimpllify/restservices/springboot_buildingblock/SpringbootBuildingblockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stacksimpllify.restservices.springboot_buildingblock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBuildingblockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
