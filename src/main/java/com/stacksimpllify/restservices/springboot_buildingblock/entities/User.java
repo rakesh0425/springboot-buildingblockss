a511f7d5aee7992eb39800b79c083ecfb93b44a0
rakesh0425 <crk.rakesh@gmail.com>
2025-03-05 16:41:54 +0530

    JsonIgnore and JsonIgnoreProperties first commit

--- src/main/java/com/stacksimpllify/restservices/springboot_buildingblock/entities/User.java
+++ src/main/java/com/stacksimpllify/restservices/springboot_buildingblock/entities/User.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 import org.springframework.hateoas.RepresentationModel;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -16,6 +19,7 @@ import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "user_details")
+@JsonIgnoreProperties({"firstname","lastname"})
 public class User extends RepresentationModel<User>{
 
 	/**
@@ -45,6 +49,7 @@ public class User extends RepresentationModel<User>{
 	private String role;
 	
 	@Column(name = "SSN",length = 50, nullable = false, unique = true)
+	@JsonIgnore
 	private String ssn;
 	
 	@OneToMany(mappedBy = "user")
